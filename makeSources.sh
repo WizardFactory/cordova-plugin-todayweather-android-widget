5f8b93ebe41640e2057feab2d541bdea70047c8a
Alec Kim <kimalec7@gmail.com>
2017-05-14 07:32:41 +0900

    source-file -> resource-file

--- makeSources.sh
+++ makeSources.sh
@@ -5,6 +5,5 @@ do
 	src=${foo#*/}
 	dst=${src#*/}
 	dst=${dst#*/}
-	dst=${dst%/*}
-	echo "<source-file src=\"$src\" target-dir=\"$dst\" />"
+	echo "<resource-file src=\"$src\" target=\"$dst\" />"
 done
