9955dd42737a21a9566782bb8dcace3262b6afa9
Alec Kim <kimalec7@gmail.com>
2017-08-05 04:06:44 +0900

    rename apppreferences to widget_preferences

--- src/android/widget/SettingsFragment.java
+++ src/android/widget/SettingsFragment.java
@@ -31,7 +31,7 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.apppreferences);
+        addPreferencesFromResource(R.xml.widget_preferences);
 
         ListPreference location = (ListPreference)findPreference("location");
         JSONArray cityListArray = SettingsActivity.loadCityListPref(getActivity());
